63f4131c45c4623b214b840ff458ff2fccdcd858
Ruchitesh <Ruchiteshmalukani@gmail.com>
2018-08-06 22:31:05 +0530

    More page with warning added !

--- webserver.js
+++ webserver.js
@@ -1,10 +1,17 @@
 const express=require('express');
 var hbs = require('hbs');
+var logmiddleware=(req,res,next)=>{
+  console.log('Log generated!');
+}
 const port=process.env.PORT || 3000;
 app=express();
 hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static('views/images')); 
 hbs.registerHelper('evalute', function(name) { return((new Date().getFullYear())+ " "+name)});
 app.set('view engine','hbs');
+app.get('/more',(req,res)=>{
+  res.render('underconstruction');
+});
 app.get('/',(req,res)=>{
   res.render('template',{
     pageTitle:"Homepage",
@@ -15,7 +22,7 @@ app.get('/help',(req,res)=>{
   res.render('template',{
     pageTitle:"Help",
     name:"Ruchitesh Malukani"
-  })
+  });
 });
 app.get('/about',(req,res)=>{
   res.render('template1',{
